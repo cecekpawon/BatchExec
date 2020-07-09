19ca5e389c2d2469da9a4f62b775fe35d80f03b1
cecekpawon <cecekpawon@users.noreply.github.com>
2020-07-09 17:14:02 +0700

    Code refactor

--- BatchExec.py
+++ BatchExec.py
@@ -6,66 +6,65 @@ import os, platform, subprocess, re, webbrowser, sublime, sublime_plugin
 #
 
 
-def glob_init():
-  global gSetting, gPath, gProg, gName, gArgs, gInline
-  global gPath, gDir, gMode, gCmd
-  global gFileName
+class Exec():
+  def init(self):
+    Setting = sublime.load_settings("BatchExec.sublime-settings")
+    Path    = Setting.get("path")
 
-  gSetting  = sublime.load_settings('BatchExec.sublime-settings')
-  gPath     = gSetting.get("path")
+    if (platform.system() == "Windows"):
+      Prog  = Path.get(Setting.get("prog"))
+    else:
+      Prog  = Path.get("tm")
 
-  if (platform.system() == "Windows"):
-    gProg   = gPath.get(gSetting.get("prog"))
-  else:
-    gProg   = gPath.get("tm")
+    self.gName      = Prog.get("name")
+    self.gArgs      = Prog.get("args")
+    self.gInline    = Prog.get("inline")
 
-  gName     = gProg.get("name")
-  gArgs     = gProg.get("args")
-  gInline   = gProg.get("inline")
+    self.gPath      = ""
+    self.gDir       = ""
+    self.gMode      = ""
+    self.gCmd       = ""
 
-  gPath     = ""
-  gDir      = ""
-  gMode     = ""
-  gCmd      = ""
+    self.gFileName  = ""
 
-  gFileName = ""
+  def args(self, **Args):
+    self.init()
 
-def glob_args(**Args):
-  global gPath, gDir, gMode, gCmd
+    self.gPath      = Args.get("paths", [])
+    self.gDir       = Args.get("dirs", [])
+    self.gMode      = Args.get("mode", "")
+    self.gCmd       = Args.get("cmd", "")
 
-  glob_init()
+  def enabled(self, **Args):
+    Result = ((platform.system() == "Windows") or (platform.system() == "Darwin"))
+    if Result:
+      self.args(**Args)
 
-  gPath  = Args.get("paths", [])
-  gDir   = Args.get("dirs", [])
-  gMode  = Args.get("mode", "")
-  gCmd   = Args.get("cmd", "")
+    return Result
 
-def glob_enabled(**Args):
-  Result = ((platform.system() == "Windows") or (platform.system() == "Darwin"))
-  if Result:
-    glob_args(**Args)
-
-  return Result
-
-def glob_repo_is_valid():
-  return bool(
-      gCmd
-      and gDir
-      and (
-        ((gMode == "svn") and os.path.exists(os.path.join(gDir[0], ".svn")))
-        or ((gMode == "git") and os.path.exists(os.path.join(gDir[0], ".git")))
+  def repo_is_valid(self):
+    return bool(
+        self.gCmd
+        and self.gDir
+        and (
+          ((self.gMode == "svn") and os.path.exists(os.path.join(self.gDir[0], ".svn")))
+          or ((self.gMode == "git") and os.path.exists(os.path.join(self.gDir[0], ".git")))
+        )
       )
-    )
 
-def glob_get_command():
-  global gCmd
+  def get_command(self):
+    if self.gCmd and self.gDir:
+      if (self.gMode == "svn"):
+        self.gCmd = " ".join([self.gMode, self.gCmd, self.gDir[0]])
+      elif (self.gMode == "git"):
+        self.gCmd = " ".join([self.gMode, "-C", self.gDir[0], self.gCmd])
 
-  if gCmd and gDir:
-    if (gMode == "svn"):
-      gCmd = " ".join([gMode, gCmd, gDir[0]])
-    elif (gMode == "git"):
-      gCmd = " ".join([gMode, "-C", gDir[0], gCmd])
 
+#
+
+bExec = Exec()
+
+#
 
 #
 # BatchExecUrlCommand - browser
@@ -75,32 +74,67 @@ def glob_get_command():
 class BatchExecUrlCommand(sublime_plugin.WindowCommand):
 
   def is_enabled(self, **Args):
-    return (glob_enabled(**Args) and glob_repo_is_valid())
+    return (bExec.enabled(**Args) and bExec.repo_is_valid())
+
+  def rep_url(self, Url, Dict):
+    for find, rep in Dict.items():
+      Url = re.sub(find, rep, Url, re.IGNORECASE)
+
+    return Url
 
   def fix_url(self, Url):
     if Url:
-      if (gMode == "svn"):
-        Url = re.sub(r"(github.com/.*?)(/branches/+)", r"\1/tree/", Url, re.IGNORECASE)  # svn co
-        Url = Url.replace("svn.code.", "") # sourceforge
 
-      elif (gMode == "git"):
-        Url = re.sub(".git$", "", Url, flags=re.IGNORECASE)
+      #
+      # svn
+      #
+      if (bExec.gMode == "svn"):
+        Dict = {
+          r"(github.com/.*?)(/branches/+)"  : r"\1/tree/",
+          r"(github.com/.*?)(/trunk/+)"     : r"\1/tree/master/",
+          "svn.code."                       : ""
+        }
 
+        Url = self.rep_url(Url, Dict)
+
+      #
+      # git
+      #
+      elif (bExec.gMode == "git"):
+        Dict = {
+          r"(.git$)"    : ""
+        }
+
+        Url = self.rep_url(Url, Dict)
+
+      #
+      # http check valid protocol
+      #
       if re.search("^http", Url, re.IGNORECASE):
-        Url = re.sub("^http:", "https:", Url, flags=re.IGNORECASE)
-        Url = re.sub("/$", "", Url)
+        Dict = {
+          r"^(http+):"  : r"\1s:",
+          r"(/$)"       : "",
+        }
+
+        Url = self.rep_url(Url, Dict)
+
+      #
+      # Non http, give em soft warn
+      #
       else:
-        Url = ""
+        Ret = sublime.ok_cancel_dialog("Open '" + Url + "' anyway?")
+        if not Ret:
+          Url = ""
 
     return Url
 
   def run(self, **Args):
-    glob_get_command()
+    bExec.get_command()
 
     Url = self.fix_url(subprocess
-                        .check_output(gCmd, shell=True)
+                        .check_output(bExec.gCmd, shell=True)
                         .strip()
-                        .decode('ascii')
+                        .decode("ascii")
                         )
 
     if Url:
@@ -115,83 +149,101 @@ class BatchExecUrlCommand(sublime_plugin.WindowCommand):
 class BatchExecCommand(sublime_plugin.WindowCommand):
 
   def is_valid(self, **Args):
-    global gFileName
-
-    gFileName = ""
+    bExec.gFileName = ""
 
     VarMap  = self.window.extract_variables()
 
-    if (gMode == "directory"):
-      if gDir:
-        gFileName = gDir[0]
-      elif gPath:
-        gFileName = os.path.dirname(gPath[0])
-      elif 'file' in VarMap:
-        gFileName = os.path.dirname(VarMap['file'])
+    #
+    # directory
+    #
+    if (bExec.gMode == "directory"):
+      if bExec.gDir:
+        bExec.gFileName = bExec.gDir[0]
+      elif bExec.gPath:
+        bExec.gFileName = os.path.dirname(bExec.gPath[0])
+      elif "file" in VarMap:
+        bExec.gFileName = os.path.dirname(VarMap["file"])
 
-    elif (gMode == "file"):
-      if not gDir:
-        if gPath:
-          gFileName = gPath[0]
-        elif 'file' in VarMap and os.path.isfile(VarMap['file']):
-          gFileName = VarMap['file']
-        if gFileName:
-          Ext = os.path.splitext(gFileName)[1][1:].strip().lower()
+    #
+    # file
+    #
+    elif (bExec.gMode == "file"):
+      if not bExec.gDir:
+        if bExec.gPath:
+          bExec.gFileName = bExec.gPath[0]
+        elif "file" in VarMap and os.path.isfile(VarMap["file"]):
+          bExec.gFileName = VarMap["file"]
+        if bExec.gFileName:
+          Ext = os.path.splitext(bExec.gFileName)[1][1:].strip().lower()
           if (platform.system() == "Windows"):
             if not Ext in ["bat", "cmd"]:
-              gFileName = ""
+              bExec.gFileName = ""
           else:
             if not Ext in ["sh", "command"]:
-              gFileName = ""
+              bExec.gFileName = ""
 
-    elif (gDir and glob_repo_is_valid()):
-      glob_get_command()
-      gFileName = gDir[0]
+    #
+    # repo
+    #
+    elif (bExec.gDir and bExec.repo_is_valid()):
+      bExec.get_command()
+      bExec.gFileName = bExec.gDir[0]
 
-    return (gFileName != "")
+    return (bExec.gFileName != "")
 
   def is_enabled(self, **Args):
-    return glob_enabled(**Args) and self.is_valid(**Args)
+    return bExec.enabled(**Args) and self.is_valid(**Args)
 
   def run(self, **Args):
-    if gFileName and os.path.exists(gFileName):
+    if bExec.gFileName and os.path.exists(bExec.gFileName):
       Drive = ""
-      Dir   = gFileName
+      Dir   = bExec.gFileName
       Cmd   = []
       Shell = False
 
       if (platform.system() == "Windows"):
         #Cmd.append("cls")
-        Drive = os.path.splitdrive(gFileName)[0]
+        Drive = os.path.splitdrive(bExec.gFileName)[0]
         Cmd.append(Drive)
 
-      if (gMode == "file"):
-        Dir = os.path.dirname(gFileName)
+      if (bExec.gMode == "file"):
+        Dir = os.path.dirname(bExec.gFileName)
 
       Cmd.append("cd " + "\"" + Dir + "\"")
 
-      if (gMode == "file"):
+      if (bExec.gMode == "file"):
         if (platform.system() == "Windows"):
-          Cmd.append(".\\\\\"" + os.path.basename(gFileName) + "\"")
+          Cmd.append(".\\\\\"" + os.path.basename(bExec.gFileName) + "\"")
         else:
-          Cmd.append("\"./" + os.path.basename(gFileName) + "\"")
-      elif gCmd:
-        Cmd.append(gCmd)
+          Cmd.append("\"./" + os.path.basename(bExec.gFileName) + "\"")
+      elif bExec.gCmd:
+        Cmd.append(bExec.gCmd)
 
-      Cmd = (" " + gInline + " ").join(Cmd)
+      Cmd = (" " + bExec.gInline + " ").join(Cmd)
 
-      if (gName.strip().lower() != "cmd"):
+      if (bExec.gName.strip().lower() != "cmd"):
         Cmd = Cmd.replace("\"", "\\\"")
 
+      #
+      # cmd-prompt
+      #
       if (platform.system() == "Windows"):
-        Cmd = " ".join([gName, gArgs, Cmd])
+        Cmd = " ".join([bExec.gName, bExec.gArgs, Cmd])
+
+      #
+      # terminal
+      #
       else:
         Shell = True
         Osa = """
           tell application "Terminal"
-            reopen
             activate
-            delay 3
+            reopen
+            repeat until busy of window 1 is false
+              delay .5
+            end repeat
+            activate
+            reopen
             tell application "System Events"
               keystroke "t" using command down
             end tell
@@ -203,4 +255,7 @@ class BatchExecCommand(sublime_plugin.WindowCommand):
 
         Cmd = (" ".join(["osascript"] + OsaArgs) % (Cmd))
 
+      #
+      # exec
+      #
       subprocess.Popen(Cmd, shell=Shell)
